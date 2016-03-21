bede8a1be8396cab2dc187b7fe19bf44cbb0ea72
RussianDaddy <sjoerd.weerstra@hotmail.com>
2016-03-21 20:50:09 +0100

    Hamburger menu
    doe is gek

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,8 @@
-
+$(function(){
+    var menu = false;
+    var hamburger = $(".hamburger");
+    $(".fa-bars").click(function(){
+        hamburger.toggleClass("menu-on","menu-off");
+        hamburger.toggleClass("hamburger-border");
+    });
+});
