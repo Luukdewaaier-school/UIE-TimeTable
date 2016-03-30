8fa0733be9f92cd4e5614671d2d7b8a3b07a8217
RussianDaddy <sjoerd.weerstra@hotmail.com>
2016-03-30 15:56:11 +0200

    Time table adapts to menu
    Het rooster beweegt nu mee als je op het hamburger icoontje drukt

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,17 @@
 $(function(){
     var menu = false;
     var hamburger = $(".hamburger");
-    $("#nav-toggle").click(function(){
+    var navToggle = $("#nav-toggle");
+    var tableContainer = $(".time-table-container");
+    var isMenuOn = false;
+
+    navToggle.click(function(){
         hamburger.toggleClass("menu-on","menu-off");
         hamburger.toggleClass("hamburger-border");
         $(this).toggleClass("active");
+
+        isMenuOn = !isMenuOn;
+        tableContainer.animate({ left:(isMenuOn) ? "13%" : "0"}, 400);
     });
 
     $("td").click(function(){
@@ -18,28 +25,4 @@ $(function(){
                 "<p class='add-para'>" + teacher + "</p>" +
                 "<p class='add-para'>" + location + "</p>";
     };
-
-    /*// overlay plugin function
-    $.fn.overlay = function() {
-
-        return this.each(function(){
-
-            /!*var $this = $(this);
-            var left = $this.offset().left;
-            var top = $this.offset().top;
-            var width = $this.outerWidth();
-            var height = $this.outerHeight();*!/
-
-            $(this).wrap("<div style='width: 90%'></div>")
-                .css('z-index', '2');
-
-        });
-    };
-
-
-// apply to single element by id
-    //$('#myBtn').overlay();
-
-// apply to all elements of class overlay
-    $('.overlay').overlay();*/
 });
